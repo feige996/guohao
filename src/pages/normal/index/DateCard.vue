ada3032ee42d5b0ff36d465e561fb3db7326706f
micropu <2001972@qq.com>
2025-09-16 18:32:41 +0800

    unocss

--- src/pages/normal/index/DateCard.vue
+++ src/pages/normal/index/DateCard.vue
@@ -3,262 +3,40 @@ defineProps(['style'])
 </script>
 
 <template>
-  <div class="group_9252">
+  <div class="relative h-[280rpx] w-[282rpx] flex flex-col items-start">
     <img
-      class="rectangle"
+      class="relative ml-[1rpx] mt-0 h-[280rpx] w-[280rpx]"
       src="https://seal-img.nos-jd.163yun.com/obj/w5rCgMKVw6DCmGzCmsK-/62601423099/86c0/5bde/f2aa/79510a8c2affe159918d9ee5caaf16e1.png"
     >
-    <div class="group_26">
-      <div class="rectangle_1">
+    <div class="relative ml-0 h-[280rpx] w-[280rpx] flex flex-col items-start -mt-[280rpx]">
+      <div class="mask-image relative ml-0 mt-0 h-[280rpx] w-[280rpx] flex flex-col items-start overflow-hidden rounded-[20rpx]">
         <img
-          class="div_1"
+          class="absolute left-[82rpx] top-1/2 h-[198rpx] w-[198rpx] -mt-[20rpx]"
           src="https://seal-img.nos-jd.163yun.com/obj/w5rCgMKVw6DCmGzCmsK-/62609437513/ad3f/4de1/f026/4cdf2770de50ddb8816207913f9f9e1c.png"
         >
       </div>
-      <div class="flexcontainer">
-        <div class="flexcontainer_1">
-          <span class="text"> 七月十五 </span><span class="text_1"> 乙己蛇年 </span>
+      <div class="relative ml-[16rpx] h-[84rpx] w-[244rpx] flex flex-row items-start -mt-[264rpx]">
+        <div class="relative ml-0 mt-0 h-[80rpx] w-[112rpx] flex flex-col items-start">
+          <span class="relative ml-0 mt-0 whitespace-pre text-center text-[28rpx] text-[#999999] font-medium leading-[40rpx]"> 七月十五 </span>
+          <span class="relative ml-0 mt-0 whitespace-pre text-center text-[28rpx] text-[#999999] font-medium leading-[40rpx]"> 乙己蛇年 </span>
         </div>
-        <div class="group_9292">
-          <span class="text_2"> 雨<br>水 </span>
+        <div class="relative ml-[84rpx] mt-0 h-[84rpx] w-[48rpx] flex flex-col items-start rounded-[16rpx] bg-[rgba(40,107,171,0.48)] text-center text-[24rpx] text-white font-medium leading-[33.6rpx]">
+          <span class="relative ml-[12rpx] mt-[8rpx]"> 雨<br>水 </span>
         </div>
       </div>
-      <div class="seal_group">
-        <span class="text_3"> 2025 </span><span class="text_4"> 09 / 06 </span>
+      <div class="relative isolate ml-[16rpx] mt-[36rpx] h-[128rpx] w-[248rpx] flex flex-col items-start rounded-[10rpx] bg-white">
+        <span class="relative z-1 ml-[14rpx] mt-[9rpx] h-[30rpx] w-[72rpx] whitespace-pre text-center text-[25rpx] text-[#286bab] font-bold leading-[30rpx]"> 2025 </span>
+        <span class="relative z-0 ml-[26rpx] mt-[15rpx] h-[88rpx] whitespace-pre text-center text-[60rpx] text-[#286bab] font-bold leading-[88rpx] -mb-[19rpx]"> 09 / 06 </span>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-* {
-  box-sizing: border-box;
-}
-.flex-row {
-  display: flex;
-  flex-direction: row;
-}
-.flex-col {
-  display: flex;
-  flex-direction: column;
-}
-.justify-start {
-  display: flex;
-  justify-content: flex-start;
-}
-.justify-center {
-  display: flex;
-  justify-content: center;
-}
-.justify-end {
-  display: flex;
-  justify-content: flex-end;
-}
-.justify-between {
-  display: flex;
-  justify-content: space-between;
-}
-.items-start {
-  display: flex;
-  align-items: flex-start;
-}
-.items-end {
-  display: flex;
-  align-items: flex-end;
-}
-.items-center {
-  display: flex;
-  align-items: center;
-}
-.no-shrink {
-  flex-shrink: 0;
-}
-</style>
-
 <style scoped>
-/* 组 9252 */
-.group_9252 {
-  position: relative;
-  width: 282rpx;
-  height: 280rpx;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
-
-/* 矩形 */
-.rectangle {
-  width: 280rpx;
-  height: 280rpx;
-  position: relative;
-  margin-top: 0;
-  margin-left: 1rpx;
-}
-
-/* 蒙版组 26 */
-.group_26 {
-  position: relative;
-  width: 280rpx;
-  height: 280rpx;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: -280rpx;
-  margin-left: 0;
-}
-
-/* 矩形 */
-.rectangle_1 {
-  position: relative;
-  width: 280rpx;
-  height: 280rpx;
-  border-radius: 20rpx;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 0;
-  margin-left: 0;
+.mask-image {
   -webkit-mask-image: url('https://seal-img.nos-jd.163yun.com/obj/w5rCgMKVw6DCmGzCmsK-/62601423099/86c0/5bde/f2aa/79510a8c2affe159918d9ee5caaf16e1.png');
   -webkit-mask-size: contain;
-}
-
-/* 热门活动-1 */
-.div_1 {
-  width: 198rpx;
-  height: 198rpx;
-  position: absolute;
-  top: 50%;
-  left: 82rpx;
-  bottom: auto;
-  margin-top: -20rpx;
-}
-
-/* flexContainer */
-.flexcontainer {
-  position: relative;
-  display: flex;
-  width: 244rpx;
-  height: 84rpx;
-  flex-direction: row;
-  align-items: flex-start;
-  margin-top: -264rpx;
-  margin-left: 16rpx;
-}
-
-/* flexContainer */
-.flexcontainer_1 {
-  position: relative;
-  display: flex;
-  width: 112rpx;
-  height: 80rpx;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-left: 0;
-  margin-top: 0;
-}
-
-/* 七月十五 */
-.text {
-  text-align: center;
-  font-size: 28rpx;
-  font-family: 'Source Han Sans';
-  font-weight: 500;
-  line-height: 40rpx;
-  color: #999999;
-  white-space: pre;
-  position: relative;
-  margin-top: 0;
-  margin-left: 0;
-}
-
-/* 乙己蛇年 */
-.text_1 {
-  text-align: center;
-  font-size: 28rpx;
-  font-family: 'Source Han Sans';
-  font-weight: 500;
-  line-height: 40rpx;
-  color: #999999;
-  white-space: pre;
-  position: relative;
-  margin-top: 0;
-  margin-left: 0;
-}
-
-/* 组 9292 */
-.group_9292 {
-  width: 48rpx;
-  height: 84rpx;
-  position: relative;
-  text-align: center;
-  font-size: 24rpx;
-  font-family: 'Source Han Sans';
-  font-weight: 500;
-  line-height: 33.6rpx;
-  color: #ffffff;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-left: 84rpx;
-  margin-top: 0;
-  border-radius: 16rpx;
-  background-color: rgba(40, 107, 171, 0.48);
-}
-
-/* 雨 水 */
-.text_2 {
-  position: relative;
-  margin-top: 8rpx;
-  margin-left: 12rpx;
-}
-
-/* Seal Group */
-.seal_group {
-  width: 248rpx;
-  height: 128rpx;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 36rpx;
-  margin-left: 16rpx;
-  border-radius: 10rpx;
-  background-color: #ffffff;
-  isolation: isolate;
-}
-
-/* 2025 */
-.text_3 {
-  width: 72rpx;
-  height: 30rpx;
-  text-align: center;
-  font-size: 25rpx;
-  font-family: 'Source Han Sans';
-  font-weight: 700;
-  line-height: 30rpx;
-  color: #286bab;
-  white-space: pre;
-  z-index: 1;
-  position: relative;
-  margin-top: 9rpx;
-  margin-left: 14rpx;
-}
-
-/* 09 / 06 */
-.text_4 {
-  text-align: center;
-  font-size: 60rpx;
-  font-family: 'Source Han Sans';
-  font-weight: 700;
-  line-height: 88rpx;
-  color: #286bab;
-  white-space: pre;
-  height: 88rpx;
-  margin-top: 15rpx;
-  margin-bottom: -19rpx;
-  z-index: 0;
-  position: relative;
-  margin-left: 26rpx;
+  mask-image: url('https://seal-img.nos-jd.163yun.com/obj/w5rCgMKVw6DCmGzCmsK-/62601423099/86c0/5bde/f2aa/79510a8c2affe159918d9ee5caaf16e1.png');
+  mask-size: contain;
 }
 </style>
