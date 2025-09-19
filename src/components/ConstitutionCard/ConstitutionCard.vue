37ae3c851642ce3b6abf8b1d23f9618d906fb0b5
micropu <2001972@qq.com>
2025-09-19 21:34:21 +0800

    暂存

--- src/components/ConstitutionCard/ConstitutionCard.vue
+++ src/components/ConstitutionCard/ConstitutionCard.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { ConstitutionCardProps, ConstitutionCardEmits } from './types'
+import type { ConstitutionCardEmits, ConstitutionCardProps } from './types'
 
 withDefaults(defineProps<ConstitutionCardProps>(), {
   constitutionType: '平和体质',
@@ -25,21 +25,21 @@ function handleAdviceClick() {
 </script>
 
 <template>
-  <div 
-    class="relative z-0 h-[280rpx] w-[334rpx] flex flex-col items-start cursor-pointer"
+  <div
+    class="relative z-0 h-[280rpx] w-[334rpx] flex flex-col cursor-pointer items-start"
     @click="handleCardClick"
   >
     <div class="relative ml-0 mt-0 h-[280rpx] w-[334rpx] flex flex-col items-start rounded-[16rpx] from-[#fff1f2] to-[#fce3e1] bg-gradient-to-br">
       <!-- 体质状态区域 -->
-      <div 
-        class="relative ml-[16rpx] mt-[16rpx] h-[40rpx] w-[288rpx] flex flex-row items-start cursor-pointer"
+      <div
+        class="relative ml-[16rpx] mt-[16rpx] h-[40rpx] w-[288rpx] flex flex-row cursor-pointer items-start"
         @click.stop="handleConstitutionClick"
       >
-        <span class="relative ml-0 mt-[8rpx] whitespace-pre text-[24rpx] text-[#ba7210] font-bold leading-[100%]">
+        <span class="relative ml-0 mt-[8rpx] whitespace-pre text-[#ba7210] font-bold text-[24rpx] leading-[100%]">
           体质状态：
         </span>
-        <div class="relative ml-[24rpx] mt-0 h-[40rpx] w-[144rpx] flex flex-col items-start whitespace-pre text-center text-[20rpx] text-white font-medium leading-[24rpx]">
-          <div 
+        <div class="relative ml-[24rpx] mt-0 h-[40rpx] w-[144rpx] flex flex-col items-start whitespace-pre text-center text-white font-medium text-[20rpx] leading-[24rpx]">
+          <div
             class="absolute bottom-0 left-0 right-0 top-0 mx-auto my-auto h-[40rpx] w-[144rpx] flex flex-col items-start bg-cover bg-center bg-no-repeat"
             :style="{ backgroundImage: `url(${badgeImageUrl})` }"
           >
@@ -49,17 +49,17 @@ function handleAdviceClick() {
           </div>
         </div>
       </div>
-      
+
       <!-- 养生建议区域 -->
       <div class="relative ml-[16rpx] mt-[16rpx] h-[192rpx] w-[302rpx] flex flex-col items-start">
-        <div 
-          class="relative ml-0 mt-0 h-[192rpx] w-[302rpx] flex flex-col items-start rounded-[8rpx] bg-white/50 cursor-pointer"
+        <div
+          class="relative ml-0 mt-0 h-[192rpx] w-[302rpx] flex flex-col cursor-pointer items-start rounded-[8rpx] bg-white/50"
           @click.stop="handleAdviceClick"
         >
-          <span class="relative ml-[18rpx] mt-[10rpx] whitespace-pre text-[24rpx] text-[#333333] font-medium leading-[100%]">
+          <span class="relative ml-[18rpx] mt-[10rpx] whitespace-pre text-[#333333] font-medium text-[24rpx] leading-[100%]">
             {{ adviceTitle }}
           </span>
-          <span class="relative ml-[16rpx] mt-[22rpx] w-[270rpx] text-justify text-[24rpx] text-[#ababab] font-medium leading-[40rpx]">
+          <span class="relative ml-[16rpx] mt-[22rpx] w-[270rpx] text-justify text-[#ababab] font-medium text-[24rpx] leading-[40rpx]">
             {{ adviceContent }}
           </span>
         </div>
--- src/components/ConsultationSection/ConsultationSection.vue
+++ src/components/ConsultationSection/ConsultationSection.vue
@@ -33,15 +33,15 @@ const props = withDefaults(defineProps<Props>(), {
 
 const emit = defineEmits<Emits>()
 
-const handleConsultClick = () => {
+function handleConsultClick() {
   emit('consultClick')
 }
 
-const handleUpgradeClick = () => {
+function handleUpgradeClick() {
   emit('upgradeClick')
 }
 
-const handleHealthManagerClick = () => {
+function handleHealthManagerClick() {
   emit('healthManagerClick')
 }
 </script>
@@ -68,18 +68,18 @@ const handleHealthManagerClick = () => {
     <div class="relative ml-0 h-[300rpx] w-[702rpx] flex-shrink-0 rounded-[16rpx] from-[#feca75] to-[#d4b477] bg-gradient-to-r -mt-[210rpx]" style="z-index: 1;" />
 
     <!-- 专业咨询标题 -->
-    <span class="relative ml-[24rpx] flex-shrink-0 whitespace-pre from-[#97493d] to-[#ba7720] bg-gradient-to-b bg-clip-text text-[40rpx] text-transparent font-bold leading-[100%] -mt-[252rpx]" style="z-index: 3;">
+    <span class="relative ml-[24rpx] flex-shrink-0 whitespace-pre from-[#97493d] to-[#ba7720] bg-gradient-to-b bg-clip-text text-transparent font-bold text-[40rpx] leading-[100%] -mt-[252rpx]" style="z-index: 3;">
       {{ props.title }}
     </span>
 
     <!-- 副标题 -->
-    <span class="relative z-2 ml-[24rpx] mt-[24rpx] flex-shrink-0 whitespace-pre text-[28rpx] text-white font-medium leading-[100%]">
+    <span class="relative z-2 ml-[24rpx] mt-[24rpx] flex-shrink-0 whitespace-pre text-white font-medium text-[28rpx] leading-[100%]">
       {{ props.subtitle }}
     </span>
 
     <!-- 健康管家消息框 -->
-    <div 
-      class="relative isolate z-5 ml-[24rpx] mt-[54rpx] h-[80rpx] w-[654rpx] flex flex-shrink-0 flex-col items-start cursor-pointer"
+    <div
+      class="relative isolate z-5 ml-[24rpx] mt-[54rpx] h-[80rpx] w-[654rpx] flex flex-shrink-0 flex-col cursor-pointer items-start"
       @click="handleHealthManagerClick"
     >
       <img
@@ -93,11 +93,11 @@ const handleHealthManagerClick = () => {
           src="@img/homepage/health-manager-icon.png"
           alt="健康管家图标"
         >
-        <span class="relative z-0 ml-[16rpx] mt-[28rpx] whitespace-pre text-[22rpx] text-[#967f64] font-normal leading-[24rpx]">
+        <span class="relative z-0 ml-[16rpx] mt-[28rpx] whitespace-pre text-[#967f64] font-normal text-[22rpx] leading-[24rpx]">
           {{ props.healthManagerMessage }}
         </span>
-        <div 
-          class="relative z-1 ml-[116rpx] mt-[12rpx] h-[56rpx] w-[148rpx] flex flex-col items-start whitespace-pre rounded-[32rpx] from-[#f0c67a] to-[#eba54b] bg-gradient-to-b text-[24rpx] text-white font-medium leading-[100%] cursor-pointer"
+        <div
+          class="relative z-1 ml-[116rpx] mt-[12rpx] h-[56rpx] w-[148rpx] flex flex-col cursor-pointer items-start whitespace-pre rounded-[32rpx] from-[#f0c67a] to-[#eba54b] bg-gradient-to-b text-white font-medium text-[24rpx] leading-[100%]"
           @click.stop="handleConsultClick"
         >
           <span class="relative ml-[38rpx] mt-[16rpx]">
@@ -108,14 +108,14 @@ const handleHealthManagerClick = () => {
     </div>
 
     <!-- 会员权益矩形 -->
-    <div 
-      class="relative isolate z-0 ml-0 mt-0 h-[98rpx] w-[702rpx] flex flex-shrink-0 flex-row items-start rounded-[16rpx] from-[#f6e2d3] to-[#ece3d1] bg-gradient-to-r cursor-pointer"
+    <div
+      class="relative isolate z-0 ml-0 mt-0 h-[98rpx] w-[702rpx] flex flex-shrink-0 flex-row cursor-pointer items-start rounded-[16rpx] from-[#f6e2d3] to-[#ece3d1] bg-gradient-to-r"
       @click="handleUpgradeClick"
     >
-      <span class="relative z-1 ml-[24rpx] mt-[50rpx] whitespace-pre text-[24rpx] text-[#ae956d] font-medium leading-[100%]">
+      <span class="relative z-1 ml-[24rpx] mt-[50rpx] whitespace-pre text-[#ae956d] font-medium text-[24rpx] leading-[100%]">
         {{ props.membershipText }}
       </span>
-      <span class="relative z-0 ml-[328rpx] mt-[50rpx] whitespace-pre text-[24rpx] text-[#ae956d] font-medium leading-[100%]">
+      <span class="relative z-0 ml-[328rpx] mt-[50rpx] whitespace-pre text-[#ae956d] font-medium text-[24rpx] leading-[100%]">
         {{ props.upgradeText }}
       </span>
       <img
