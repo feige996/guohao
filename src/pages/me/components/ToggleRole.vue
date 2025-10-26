089eeb672a5b06da1c1e9374deac185cacee6135
feige996 <1020102647@qq.com>
2025-10-26 15:01:42 +0800

    feat(角色切换): 添加切换角色后跳转到个人中心页面的功能
    在切换角色后自动跳转到个人中心页面，提升用户体验

--- src/pages/me/components/ToggleRole.vue
+++ src/pages/me/components/ToggleRole.vue
@@ -2,13 +2,23 @@
 import { useUserStore } from '@/store'
 
 const userStore = useUserStore()
+
+function handleToggleRole() {
+  userStore.toggleUserDefaultRole()
+  // 还需要设置当前 tabbar 切换的对应的路由
+  uni.switchTab({
+    url: '/pages/me/index',
+  })
+}
 </script>
 
 <template>
-  <view>
-    <view>当前角色：{{ userStore.userDefaultRole }}</view>
-    <wd-button type="primary" @click="userStore.toggleUserDefaultRole">
-      切换角色
-    </wd-button>
+  <view class="text-center">
+    <view class="mx-auto mt-2 inline-flex items-center gap-3">
+      <view>当前角色：{{ userStore.userDefaultRole }}</view>
+      <wd-button type="primary" @click="handleToggleRole">
+        切换角色
+      </wd-button>
+    </view>
   </view>
 </template>
