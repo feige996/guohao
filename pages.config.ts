fd583759bf96510a4bd01d8cfcd386d2aef267dc
feige996 <1020102647@qq.com>
2025-10-26 14:24:29 +0800

    feat(配置): 更新分包配置和页面路由
    移除冗余的页面路由配置，将 TUIKit 添加到分包配置中
    简化预加载规则，只保留必要的触发页面配置

--- pages.config.ts
+++ pages.config.ts
@@ -3,32 +3,6 @@ import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 import { tabBar } from './src/tabbar/config'
 
 export default defineUniPages({
-  pages: [
-    {
-      path: 'pages/shop/cart',
-      style: {},
-    },
-    {
-      path: 'pages/normal/index/appointmentConsult',
-      style: {
-        navigationBarTitleText: '我的预约',
-      },
-    },
-    {
-      path: 'pages/normal/index/healthConsult',
-      style: {
-        navigationBarTitleText: '健康问答',
-      },
-    },
-    {
-      path: 'pages/normal/me/components/ServiceCard/ConsultationRecord',
-      style: {},
-    },
-    {
-      path: 'pages/normal/me/components/ServiceCard/AddressManagement',
-      style: {},
-    },
-  ],
   globalStyle: {
     navigationStyle: 'custom',
     backgroundColor: '#f6e2d3',
@@ -43,52 +17,10 @@ export default defineUniPages({
         'z-paging/components/z-paging$1/z-paging$1.vue',
     },
   },
-  // TUIKit 分包配置
-  subPackages: [
-    {
-      root: 'TUIKit',
-      pages: [
-        {
-          path: 'components/TUIConversation/index',
-          style: {},
-        },
-        {
-          path: 'components/TUIChat/index',
-          style: {},
-        },
-        // 集成 chat 组件，必须配置该路径: 视频播放
-        {
-          path: 'components/TUIChat/video-play',
-          style: {},
-        },
-        {
-          path: 'components/TUIChat/web-view',
-          style: {},
-        },
-        {
-          path: 'components/TUIContact/index',
-          style: {},
-        },
-        {
-          path: 'components/TUIGroup/index',
-          style: {},
-        },
-        {
-          path: 'components/TUISearch/index',
-          style: {},
-        },
-      ],
-    },
-  ],
   // 预加载规则
   preloadRule: {
   // 可以配置多个触发页面
-    'pages/normal/index/index': {
-      network: 'all',
-      packages: ['TUIKit'],
-    },
-    // 可以配置多个触发页面
-    'pages/doctor/index/index': {
+    'pages/index/index': {
       network: 'all',
       packages: ['TUIKit'],
     },
