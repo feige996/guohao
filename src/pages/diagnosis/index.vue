191fadc66ac1ba47682fcd08d197f1725529b26b
feige996 <1020102647@qq.com>
2025-10-26 13:13:52 +0800

    feat(诊断): 重构诊断模块目录结构并新增组件
    将医生诊断相关组件从pages/doctor/diagnosis迁移至src/pages/diagnosis/components/DiagnosisDoctor目录
    新增诊断用户端组件MedicalServiceCards
    实现图文问诊、语音问诊、视频问诊功能
    添加药材名单和处方使用功能

---
+++ src/pages/diagnosis/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { useUserStore } from '@/store/userStore'
+import DiagnosisDoctor from './components/DiagnosisDoctor/index.vue'
+import DiagnosisUser from './components/DiagnosisUser/index.vue'
+
+const userStore = useUserStore()
+
+definePage({
+  style: {
+    navigationBarTitleText: '首页',
+  },
+})
+</script>
+
+<template>
+  <view class="home">
+    <DiagnosisDoctor v-if="userStore.isDoctor" />
+    <DiagnosisUser v-else />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
