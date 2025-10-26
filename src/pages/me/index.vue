98b458c8bdca98a0dc5f2f3cf1b572595be1d8c8
feige996 <1020102647@qq.com>
2025-10-26 13:06:51 +0800

    feat(me): 重构个人中心页面结构，迁移医生和用户模块组件
    将原有的医生和个人用户页面组件迁移至统一的me目录下，重构组件结构
    新增我的服务、VIP卡片、菜单卡片等功能组件
    优化用户信息展示和交互逻辑，统一代码风格和样式

---
+++ src/pages/me/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { useUserStore } from '@/store/userStore'
+import MeDoctor from './components/MeDoctor/index.vue'
+import MeUser from './components/MeUser/index.vue'
+
+const userStore = useUserStore()
+
+definePage({
+  style: {
+    navigationBarTitleText: '我的',
+  },
+})
+</script>
+
+<template>
+  <view class="home">
+    <MeDoctor v-if="userStore.isDoctor" />
+    <MeUser v-else />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
