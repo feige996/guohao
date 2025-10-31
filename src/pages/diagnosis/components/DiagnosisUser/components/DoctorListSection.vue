4661326c6851526ae235cb362d912e1de1f593c6
feige996 <1020102647@qq.com>
2025-10-31 16:17:51 +0800

    feat(诊断用户): 添加医生列表卡片样式和内容
    实现医生信息卡片的基本样式布局，包括医生姓名、职称和医院信息

--- src/pages/diagnosis/components/DiagnosisUser/components/DoctorListSection.vue
+++ src/pages/diagnosis/components/DiagnosisUser/components/DoctorListSection.vue
@@ -3,11 +3,17 @@
 </script>
 
 <template>
-  <view class="">
-    ni
+  <view class="rounded-lg bg-white px-4 py-3">
+    <view class="flex items-center gap-1 text-[15px] font-500">
+      <view class="text-[#333333]">
+        陈医生
+      </view>
+      <view class="text-[#999999]">
+        主任医师
+      </view>
+    </view>
+    <view class="mt-2">
+      北京第一医院
+    </view>
   </view>
 </template>
-
-<style lang="scss" scoped>
-//
-</style>
