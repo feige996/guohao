de5e392bbc9bee32041dbeb79631f9a924555107
feige996 <1020102647@qq.com>
2025-11-01 18:35:56 +0800

    refactor(router): 更新登录和注册页面路径
    将登录页面路径从'/pages/login/login'改为'/pages/login/password-login'，注册页面路径从'/pages-fg/login/register'改为'/pages/login/sms-login'，以更清晰地反映登录方式

--- src/router/config.ts
+++ src/router/config.ts
@@ -8,8 +8,8 @@ export const LOGIN_STRATEGY_MAP = {
 export const LOGIN_STRATEGY = LOGIN_STRATEGY_MAP.DEFAULT_NO_NEED_LOGIN
 export const isNeedLoginMode = LOGIN_STRATEGY === LOGIN_STRATEGY_MAP.DEFAULT_NEED_LOGIN
 
-export const LOGIN_PAGE = '/pages/login/login'
-export const REGISTER_PAGE = '/pages-fg/login/register'
+export const LOGIN_PAGE = '/pages/login/password-login'
+export const REGISTER_PAGE = '/pages/login/sms-login'
 export const NOT_FOUND_PAGE = '/pages-fg/404/index'
 
 export const LOGIN_PAGE_LIST = [LOGIN_PAGE, REGISTER_PAGE]
